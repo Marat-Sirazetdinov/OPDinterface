a9ec4f04fcca615a469fa65942d635d96117f821
Marat-Sirazetdinov <kakahavupvup20005@gmail.com>
2026-04-12 22:06:04 +0300

    Delete project directory

--- project/project/logic/__init__.py
+++
@@ -1,9 +0,0 @@
-from .seismic import reorder_pipeline, validate_seismic_file
-from .worker import LOGIC_STOP, logic_worker_main
-
-__all__ = [
-    "LOGIC_STOP",
-    "logic_worker_main",
-    "reorder_pipeline",
-    "validate_seismic_file",
-]
